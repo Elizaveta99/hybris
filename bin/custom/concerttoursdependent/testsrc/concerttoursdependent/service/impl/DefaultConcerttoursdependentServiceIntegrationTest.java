ecbeb6177d35c368604c27dc95e6d3265e934364
Lizaveta Rudzko <30577965+Elizaveta99@users.noreply.github.com>
2021-01-19 13:58:21 +0300

    Task2 (#3)
    Create a New Extension , connection between extensions, .gitignore fixes

---
+++ bin/custom/concerttoursdependent/testsrc/concerttoursdependent/service/impl/DefaultConcerttoursdependentServiceIntegrationTest.java
@@ -0,0 +1,72 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package concerttoursdependent.service.impl;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static concerttoursdependent.constants.ConcerttoursdependentConstants.PLATFORM_LOGO_CODE;
+
+import de.hybris.bootstrap.annotations.IntegrationTest;
+import de.hybris.platform.core.model.media.MediaModel;
+import de.hybris.platform.servicelayer.ServicelayerBaseTest;
+import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
+import de.hybris.platform.servicelayer.search.FlexibleSearchService;
+
+import javax.annotation.Resource;
+
+import org.junit.Before;
+import org.junit.Test;
+import concerttoursdependent.service.ConcerttoursdependentService;
+import concerttoursdependent.service.impl.DefaultConcerttoursdependentService;
+
+
+/**
+ * This is an example of how the integration test should look like. {@link ServicelayerBaseTest} bootstraps platform so
+ * you have an access to all Spring beans as well as database connection. It also ensures proper cleaning out of items
+ * created during the test after it finishes. You can inject any Spring service using {@link Resource} annotation. Keep
+ * in mind that by default it assumes that annotated field name matches the Spring Bean ID.
+ */
+@IntegrationTest
+public class DefaultConcerttoursdependentServiceIntegrationTest extends ServicelayerBaseTest
+{
+	@Resource
+	private ConcerttoursdependentService concerttoursdependentService;
+	@Resource
+	private FlexibleSearchService flexibleSearchService;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		concerttoursdependentService.createLogo(PLATFORM_LOGO_CODE);
+	}
+
+	@Test
+	public void shouldReturnProperUrlForLogo() throws Exception
+	{
+		// given
+		final String logoCode = "concerttoursdependentPlatformLogo";
+
+		// when
+		final String logoUrl = concerttoursdependentService.getHybrisLogoUrl(logoCode);
+
+		// then
+		assertThat(logoUrl).isNotNull();
+		assertThat(logoUrl).isEqualTo(findLogoMedia(logoCode).getURL());
+	}
+
+	private MediaModel findLogoMedia(final String logoCode)
+	{
+		final FlexibleSearchQuery fQuery = new FlexibleSearchQuery("SELECT {PK} FROM {Media} WHERE {code}=?code");
+		fQuery.addQueryParameter("code", logoCode);
+
+		return flexibleSearchService.searchUnique(fQuery);
+	}
+
+}

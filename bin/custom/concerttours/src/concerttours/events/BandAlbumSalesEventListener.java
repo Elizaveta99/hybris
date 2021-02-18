c957a09365b176fd18e32f08686703f2b7468ae4
ElizavetaRudko <eliz.vitebsk@gmail.com>
2021-02-18 18:03:03 +0300

    Make News type catalog aware

--- bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEventListener.java
+++ bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEventListener.java
@@ -2,7 +2,9 @@ package concerttours.events;
 
 import concerttours.constants.ConcerttoursConstants;
 import concerttours.model.NewsModel;
+import de.hybris.platform.catalog.CatalogVersionService;
 import de.hybris.platform.servicelayer.event.impl.AbstractEventListener;
+import de.hybris.platform.servicelayer.keygenerator.KeyGenerator;
 import de.hybris.platform.servicelayer.model.ModelService;
 
 import java.util.Date;
@@ -10,6 +12,8 @@ import java.util.Date;
 public class BandAlbumSalesEventListener extends AbstractEventListener<BandAlbumSalesEvent> {
 
     private ModelService modelService;
+    private KeyGenerator keyGenerator;
+    private CatalogVersionService catalogVersionService;
 
     public ModelService getModelService() {
         return modelService;
@@ -19,12 +23,22 @@ public class BandAlbumSalesEventListener extends AbstractEventListener<BandAlbum
         this.modelService = modelService;
     }
 
+    public void setKeyGenerator(KeyGenerator keyGenerator) {
+        this.keyGenerator = keyGenerator;
+    }
+
+    public void setCatalogVersionService(CatalogVersionService catalogVersionService) {
+        this.catalogVersionService = catalogVersionService;
+    }
+
     @Override
     protected void onEvent(BandAlbumSalesEvent event) {
         if (event != null) {
             String headline = String.format(ConcerttoursConstants.BAND_SALES_HEADLINE, event.getName());
             String content = String.format(ConcerttoursConstants.BAND_SALES_CONTENT, event.getName(), event.getSales());
             NewsModel news = modelService.create(NewsModel.class);
+            news.setId(keyGenerator.generate().toString());
+            news.setCatalogVersion(catalogVersionService.getCatalogVersion(ConcerttoursConstants.CATALOG_ID, ConcerttoursConstants.CATALOG_VERSION_NAME));
             news.setDate(new Date());
             news.setHeadline(headline);
             news.setContent(content);
